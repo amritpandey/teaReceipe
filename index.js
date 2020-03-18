ae6ce3291acd9b27e6f0d9ee5963ee6adbaf125f
amritpandey <amrit.pandey111@gmail.com>
2020-03-18 07:07:18 +0100

    initial tea receipe files

---
+++ index.js
@@ -0,0 +1,14 @@
+function ingredientsHover(){
+  document.getElementById('ingredients').firstElementChild.style.fontSize="200%";
+}
+
+function preparationHover(){
+  document.getElementById('preparation').firstElementChild.style.fontSize="200%";
+}
+function ingredientsNormal(){
+  document.getElementById('ingredients').firstElementChild.style.fontSize="100%";
+}
+
+function preparationNormal(){
+  document.getElementById('preparation').firstElementChild.style.fontSize="100%";
+}
